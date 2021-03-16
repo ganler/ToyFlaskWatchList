7ef2b8dc735f257106bd532374f2b1aa97f49ec1
ganler <jaway.liu@gmail.com>
2021-03-16 21:33:24 +0800

    feat: mk app integrated with DB.

--- app.py
+++ app.py
@@ -1,9 +1,42 @@
+import os
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+import click
 
 app = Flask(__name__)  # File name.
 
-@app.route('/')
-def index():
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(app.root_path, "app.sqlite")}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+db = SQLAlchemy(app)  # Remember to do `python mkdb.py`.
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+        click.echo('Old DB destroyed.')
+    db.create_all()
+    click.echo('New DB created.')  # 输出提示信息
+
+
+@app.cli.command()
+def forge():
+    db.create_all()
     name = 'Jiawei Liu'
     movies = [
         {'title': 'My Neighbor Totoro', 'year': '1988'},
@@ -17,7 +50,22 @@ def index():
         {'title': 'WALL-E', 'year': '2008'},
         {'title': 'The Pork of Music', 'year': '2012'},
     ]
-    return render_template('index.html', name=name, movies=movies)
+
+    user = User(name=name)
+    db.session.add(user)  # Add first, and then commit.
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Forge complete.')
+
+
+
+@app.route('/')
+def index():
+    User.query.first()
+    return render_template('index.html', user=User.query.first(), movies=Movie.query.all())
 
 """
 * `flask run` to run the app (w/o the __main__ function scope).
